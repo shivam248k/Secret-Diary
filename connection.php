0025fa70c8514ffcc2e5a2e4c6054b8285cbdcfc
Shivam Kumar Sharma <80200591+shivam248k@users.noreply.github.com>
2021-09-13 21:59:43 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = mysqli_connect("localhost", "root","", "shivam24");
+    $link = mysqli_connect("hostname", "username","password", "databasename");
         
         if (mysqli_connect_error()) {
             
