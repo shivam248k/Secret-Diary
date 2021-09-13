255c64a7e59837c6f1da7c0d9ac334a59b5917fc
Shivam Kumar Sharma <80200591+shivam248k@users.noreply.github.com>
2021-09-13 21:43:24 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+  <head>
+        
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Secret Diary </title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+      
+      <style type="text/css">
+          
+      .container {
+      
+            text-align: center;
+            width: 400px;
+      }
+          
+      #homePageContainer {
+              
+            margin-top: 190px;
+          
+      }
+          
+          #containerLoggedInPage {
+              
+              margin-top: 70px;
+              
+          }
+          
+          
+      html { 
+          
+          background: url(background.jpg) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          
+          }
+          
+          body {
+              
+              background: none;
+              color: #FFF;
+              
+          }
+          
+          #logInForm {
+              
+              display:none;
+              
+          }
+          
+          .toggleForms {
+              
+              font-weight: bold;
+              
+          }
+          
+          #diary {
+            height: 660px;
+              width: 1505px;
+          }
+           .cp-text{
+            position: fixed;     
+            text-align: center;    
+            bottom: 15px; 
+            width: 100%;
+            
+                }
+          
+
+              
+          
+          
+      </style>
+  </head>
+  <body>
+
+  
+  <div class="cp-text">© Copyright 2021 By Shivam.</div>
+        
